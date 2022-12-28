d17ce1e77bd6926f00054f363aad26721a3c76e9
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-28 17:15:07 -0300

    LEARN: Trabalhando com eventos

---
+++ 06.eventos/src/Form.jsx
@@ -0,0 +1,20 @@
+function cadastrarUsuario(e){
+    e.preventDefault()
+    window.alert("Usuário cadastrado com sucesso!");
+}
+
+export default function Form() {
+    return (
+        <div>
+            <h1>Meu cadastro</h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <input type="text" placeholder="Digite o seu nome"/>
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar"/>
+                </div>
+            </form>
+        </div>
+    )
+}
