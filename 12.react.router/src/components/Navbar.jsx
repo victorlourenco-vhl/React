3c6727038e34068ffa897b904e27b99337cc482c
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-01-04 16:48:30 -0300

    FEAT: Corringindo a rota de navecao do /users

--- 12.react.router/src/components/Navbar.jsx
+++ 12.react.router/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to="/contato">Contato</Link></li>
-                <li><Link to="users">Users</Link></li>
+                <li><Link to="/users">Users</Link></li>
             </ul>
         </nav>
     )
