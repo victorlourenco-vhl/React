a249b1102801969aba53186d869f75d955d992f8
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-01-03 21:41:04 -0300

    LEAR: React Icons

---
+++ 13.react.icons/src/Footer.jsx
@@ -0,0 +1,14 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
+export default function Footer(){
+    return (
+        <footer>
+            <ul>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+        </footer>
+        
+    )
+}
